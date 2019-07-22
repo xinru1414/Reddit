d4e6396f51fbae80d57cf89238c62bde3076a336
Xinru Yan <xinruyan@erebor.lti.cs.cmu.edu>
2019-07-22 14:30:33 -0400

    Functional Schemas

--- FunctionalSchemas/src/Baselines/config.py
+++
@@ -1,8 +0,0 @@
-train_path = '../data/reddit/train_with_schema.jsonl'
-dev_path = '../data/reddit/dev_with_schema.jsonl'
-test_path = '../data/reddit/test_with_schema.jsonl'
-model_path = '../model/reddit/'
-movie_schemas = [[2, 4, 8], [1, 4], [1, 2, 4, 6, 8], [1, 3, 5, 9], [5, 7, 8, 9]]
-reddit_schemas = [[3, 4, 5, 7], [5, 6, 7, 9], [3, 4, 5],[3, 4, 7, 9], [2, 3, 4, 6]]
-mode = 'nonschema'
-
